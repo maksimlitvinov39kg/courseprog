5b5444a8adaaf79ff6c8729262ef478b404a53b0
Maksim $WAGGoff <95618433+maksimlitvinov39kg@users.noreply.github.com>
2023-04-24 14:00:35 +0300

    Add files via upload

---
+++ CourseProgApplication.java
@@ -0,0 +1,13 @@
+package com.example.courseprog;
+
+import   org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  CourseProgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseProgApplication.class, args);
+    }
+
+}
